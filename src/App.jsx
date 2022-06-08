06182a1ce7e726411b0e15df9796dc8423f59408
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-06-08 18:40:35 -0300

    Inicio do projeto Ignite Feed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Post } from './Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/SIdebar';
+import './global.css';
+
+import styles from './App.module.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author= "Igor Guimarães Veludo" 
+            content= "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et iure quam rem beatae autem iste ex ad nihil quae facere officia, ullam dolor eaque ipsa incidunt exercitationem perspiciatis optio porro."
+          />
+        </main>
+      </div>
+    </div>
+  )
+}
+
